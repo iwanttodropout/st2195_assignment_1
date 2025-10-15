9ff04d67a2a98085f06f693e4680a90c85234b6c
Chin Yi Jing <chinyijing06@gmail.com>
2025-10-15 15:57:27 +0800

    Initial commit: add README, R and Python files for assignment

--- r_studio/print_numbers.R
+++ r_studio/print_numbers.R
@@ -1,4 +1,30 @@
 # Program to print all integers between 1 and 10 (including 1 and 10) to screen
-for (i in 1:10) {
-  print(i)
+
+cat(1,2,3,4,5,6,7,8,9,10)
+
+#Method 2: print numbers 1 to 10
+print (1:10)
+
+#Method 3: print numbers 1 to 10 using a "for" loop
+
+for (x in 1:10) {
+  print(x)
+  print("------")
 }
+
+for (x in c(1:10)) {
+  print(x)
+}
+
+for (x in c(1:10)) {
+  cat(x, " ")
+}
+
+#Method 4: print numbers 1 to 10 using a "while loop"
+
+x <- 1
+while (x <= 10) {
+  print (x)
+  x <- x + 1
+}
+  
